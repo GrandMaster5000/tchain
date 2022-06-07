b5bf3bea69c0fd5551772a4c553e9fb68bcc6e34
Коробских Анатолий <ggamer556@mail.ru>
2022-06-07 21:58:30 +0300

    #WIP blockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -3,13 +3,14 @@ package blockchain
 import (
 	"crypto/rsa"
 	"database/sql"
+	"encoding/base64"
+	"encoding/json"
 	"os"
 	"time"
 )
 
 type BlockChain struct {
-	DB    *sql.DB
-	index uint64
+	DB *sql.DB
 }
 
 type Block struct {
@@ -25,19 +26,40 @@ type Block struct {
 }
 
 type Transaction struct {
-	RandomBytes []byte
-	PrevBlock   []byte
-	Sender      string
-	Receiver    string
-	Value       uint64
-	ToStorage   uint64
-	CurrHash    []byte
-	Signature   []byte
+	RandBytes []byte
+	PrevBlock []byte
+	Sender    string
+	Receiver  string
+	Value     uint64
+	ToStorage uint64
+	CurrHash  []byte
+	Signature []byte
 }
 
 type User struct {
 	PrivateKey *rsa.PrivateKey
 }
+
+const (
+	CREATE_TABLE = `
+CREATE TABLE BlockChain (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Hash VARCHAR(44) UNIQUE,
+    Block TEXT
+);
+`
+)
+
+const (
+	DIFFICULTY = 20
+)
+
+const (
+	GENESIS_BLOCK  = "GENESIS-BLOCK"
+	STORAGE_VALUE  = 100
+	GENESIS_REWARD = 100
+	STORAGE_CHAIN  = "STORAGE-CHAIN"
+)
 
 func NewChain(filename, receiver string) error {
 	file, err := os.Create(filename)
@@ -50,18 +72,81 @@ func NewChain(filename, receiver string) error {
 		return err
 	}
 	defer db.Close()
-	_, err = db.Exec(CREATE_TABLE)
+	db.Exec(CREATE_TABLE)
 	chain := &BlockChain{
 		DB: db,
 	}
 	genesis := &Block{
-		CurrHash:  []byte(GENESIS_BLOCK),
+		PrevHash:  []byte(GENESIS_BLOCK),
 		Mapping:   make(map[string]uint64),
 		Miner:     receiver,
 		TimeStamp: time.Now().Format(time.RFC3339),
 	}
 	genesis.Mapping[STORAGE_CHAIN] = STORAGE_VALUE
 	genesis.Mapping[receiver] = GENESIS_REWARD
+	genesis.CurrHash = genesis.hash()
 	chain.AddBlock(genesis)
 	return nil
+}
+
+func LoadChain(filename string) *BlockChain {
+	db, err := sql.Open("sqlite3", filename)
+	if err != nil {
+		return nil
+	}
+	chain := &BlockChain{
+		DB: db,
+	}
+	return chain
+}
+
+func NewBlock(miner string, prevHash []byte) *Block {
+	return &Block{
+		Difficulty: DIFFICULTY,
+		PrevHash:   prevHash,
+		Miner:      miner,
+		Mapping:    make(map[string]uint64),
+	}
+}
+
+func NewTransaction(user *User, lasthash []byte, to string, value uint64) *Transaction {
+	tx := &Transaction{
+		RandBytes: GenerateRandomBytes(RAND_BYTES),
+		PrevBlock: lasthash,
+		Sender:    user.Address(),
+		Receiver:  to,
+		Value:     value,
+	}
+	if value > START_PERCENT {
+		tx.ToStorage = STORAGE_REWARD
+	}
+	tx.CurrHash = tx.hash()
+	tx.Signature = tx.sign(user.Private())
+	return tx
+}
+
+func (chain *BlockChain) Size() uint64 {
+	var size uint64
+	row := chain.DB.QueryRow("SELECT Id FROM BlockChain ORDER BY Id DESC")
+	row.Scan(&size)
+	return size
+}
+
+func (chain *BlockChain) AddBlock(block *Block) {
+	chain.DB.Exec("INSERT INTO BlockChain (Hash, Block) VALUES ($1, $2)",
+		Base64Encode(block.CurrHash),
+		SerializeBlock(block),
+	)
+}
+
+func Base64Encode(data []byte) string {
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+func SerializeBlock(block *Block) string {
+	jsonData, err := json.MarshalIndent(*block, "", "\t")
+	if err != nil {
+		return ""
+	}
+	return string(jsonData)
 }
