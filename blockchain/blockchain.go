e04ac798b231e56312eebe51ad7f38fc9086c1c7
Коробских Анатолий <ggamer556@mail.ru>
2022-06-08 11:10:16 +0300

    #WIP blockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,10 +1,17 @@
 package blockchain
 
 import (
+	"bytes"
+	"crypto"
+	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
 	"database/sql"
 	"encoding/base64"
+	"encoding/binary"
 	"encoding/json"
+	"errors"
 	"os"
 	"time"
 )
@@ -51,7 +58,10 @@ CREATE TABLE BlockChain (
 )
 
 const (
-	DIFFICULTY = 20
+	DIFFICULTY     = 20
+	RAND_BYTES     = 32
+	START_PERCENT  = 10
+	STORAGE_REWARD = 1
 )
 
 const (
@@ -123,6 +133,84 @@ func NewTransaction(user *User, lasthash []byte, to string, value uint64) *Trans
 	tx.CurrHash = tx.hash()
 	tx.Signature = tx.sign(user.Private())
 	return tx
+}
+
+func (block *Block) AddTransaction(chain *BlockChain, tx *Transaction) error {
+	if tx == nil {
+		return errors.New("tx is null")
+	}
+	if tx.Value == 0 {
+		return errors.New("tx value = 0")
+	}
+	if len(block.Transactions) == TXS_LIMIT && tx.Sender != STORAGE_CHAIN {
+		return errors.New("len tx = limit")
+	}
+	return nil
+}
+
+func (user *User) Address() string {
+	return StringPublic(user.Public())
+}
+
+func (user *User) Private() *rsa.PrivateKey {
+	return user.PrivateKey
+}
+
+func (tx *Transaction) hash() []byte {
+	return HashSum(bytes.Join(
+		[][]byte{
+			tx.RandBytes,
+			tx.PrevBlock,
+			[]byte(tx.Sender),
+			[]byte(tx.Receiver),
+			ToBytes(tx.Value),
+			ToBytes(tx.ToStorage),
+		},
+		[]byte{},
+	))
+}
+
+func (tx *Transaction) sign(priv *rsa.PrivateKey) []byte {
+	return Sign(priv, tx.CurrHash)
+}
+
+func StringPublic(pub *rsa.PublicKey) string {
+	return Base64Encode(x509.MarshalPKCS1PublicKey(pub))
+}
+
+func (user *User) Public() *rsa.PublicKey {
+	return &(user.PrivateKey).PublicKey
+}
+
+func HashSum(data []byte) []byte {
+	hash := sha256.Sum256(data)
+	return hash[:]
+}
+
+func ToBytes(num uint64) []byte {
+	var data = new(bytes.Buffer)
+	err := binary.Write(data, binary.BigEndian, num)
+	if err != nil {
+		return nil
+	}
+	return data.Bytes()
+}
+
+func Sign(priv *rsa.PrivateKey, data []byte) []byte {
+	signdata, err := rsa.SignPSS(rand.Reader, priv, crypto.SHA256, data, nil)
+	if err != nil {
+		return nil
+	}
+	return signdata
+}
+
+func GenerateRandomBytes(max uint) []byte {
+	var slice = make([]byte, max)
+	_, err := rand.Read(slice)
+	if err != nil {
+		return nil
+	}
+	return slice
 }
 
 func (chain *BlockChain) Size() uint64 {
